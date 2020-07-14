fe5296565806bb2c5a660ceb1a36426996d89670
stephen grider <ste.grider@gmail.com>
2020-07-14 16:39:19 -0600

    update config

--- pool.js
+++ pool.js
@@ -3,11 +3,11 @@ const { Pool } = require('pg');
 const config =
   process.env.NODE_ENV === 'production'
     ? {
-        user: 'root',
-        host: 'localhost',
-        database: 'postgres',
-        password: '',
-        port: 5432,
+        user: process.env.PG_USER,
+        host: process.env.PG_HOST,
+        database: process.env.PG_DB,
+        password: process.env.PG_PASSWORD,
+        port: process.env.PG_PORT,
       }
     : {
         user: 'sg',
